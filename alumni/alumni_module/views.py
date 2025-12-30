6626bda6ecaf43387661015595ac8f1bfa65c793
arjuuh <arjunashok172@gmail.com>
2025-12-30 13:51:32 +0530

    updated

--- alumni/alumni_module/views.py
+++ alumni/alumni_module/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import render, redirect
 from .models import Alumni
 
@@ -8,12 +9,14 @@ def register(request):
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
+        password = make_password(request.POST['password'])
         batch = request.POST['batch']
         department = request.POST['department']
 
         Alumni.objects.create(
             name=name,
             email=email,
+            password=password,
             batch=batch,
             department=department,
             is_verified=False
@@ -21,6 +24,7 @@ def register(request):
         return redirect('/')
 
     return render(request, 'register.html')
+
 
 def alumni_list(request):
     alumni = Alumni.objects.filter(is_verified=True)
@@ -31,15 +35,20 @@ def alumni_login(request):
 
     if request.method == 'POST':
         email = request.POST['email']
+        password = request.POST['password']
 
         try:
             alumni = Alumni.objects.get(email=email, is_verified=True)
-            request.session['alumni_id'] = alumni.id
-            return redirect('/dashboard/')
+            if check_password(password, alumni.password):
+                request.session['alumni_id'] = alumni.id
+                return redirect('/dashboard/')
+            else:
+                error = "Invalid email or password"
         except Alumni.DoesNotExist:
-            error = "Invalid email or alumni not verified"
+            error = "Invalid email or password or not verified"
 
     return render(request, 'alumni_login.html', {'error': error})
+
 
 def dashboard(request):
     alumni_id = request.session.get('alumni_id')
